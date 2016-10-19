7fafd5865b714502acaeebf906210740820cef86
Christopher Hummersone <c.hummersone@surrey.ac.uk>
2016-10-19 17:53:09 +0100

    Moved implementation to bottom of file. Added copy constructor and assignment overload. Changed biquad* member to unique_ptr<> with specific deleter. Throw exceptions when biquad_create() or biquad_clone() return NULL.

--- biquads.hpp
+++ biquads.hpp
@@ -1,6 +1,8 @@
 #ifndef BIQUADS_HPP_
 #define BIQUADS_HPP_
 
+#include <memory>
+#include <new>
 #include "biquads.h"
 
 class biquadFilter {
@@ -14,41 +16,133 @@ public:
         double fc = 1000,
         double Q = 0.7071,
         double peakGain = 0
-    ) {
-        b = biquad_create(type, fs, fc, Q, peakGain);
-    }
+    );
     
     // destructor
-    ~biquadFilter(void) { biquad_destroy(b); }
+    ~biquadFilter(void) = default;
+    
+    // copy constructor
+    biquadFilter(const biquadFilter& other);
+    
+    // assignment
+    biquadFilter& operator=(const biquadFilter& other);
     
     // calculate magnitude
-    double magnitude(double f) { return biquad_magnitude(b, f); }
+    double magnitude(double f) const;
     
     // process sample
-    bqFloat process(bqFloat x) { return biquad_process(b, x); }
+    bqFloat process(bqFloat x);
     
-    // set filter parameter - fs
-    void setFs(double fs) { return biquad_setFs(b, fs); }
+    // set/get filter parameter - fs
+    void setFs(double fs);
+    double getFs(void) const;
     
-    // set filter parameter - type
-    void setType(bqFilterType type) { biquad_setType(b, type); }
+    // set/get filter parameter - type
+    void setType(bqFilterType type);
+    bqFilterType getType(void) const;
     
-    // set filter parameter - fc
-    void setFc(double fc) { biquad_setFc(b, fc); }
+    // set/get filter parameter - fc
+    void setFc(double fc);
+    double getFc(void) const;
     
-    // set filter parameter - Q
-    void setQ(double Q) { biquad_setQ(b, Q); }
+    // set/get filter parameter - Q
+    void setQ(double Q);
+    double getQ(void) const;
     
-    // set filter parameter - peakGain
-    void setPeakGain(double peakGain) { biquad_setPeakGain(b, peakGain); }
+    // set/get filter parameter - peakGain
+    void setPeakGain(double peakGain);
+    double getPeakGain(void) const;
     
     // clear buffers
-    void clear(void) { biquad_clear(b); }
+    void clear(void);
 
 private:
     
-    biquad *b;
+    std::unique_ptr<biquad, void(*)(biquad*)> b;
 
 };
+
+// Implementation
+
+// constructor
+biquadFilter::biquadFilter(
+    bqFilterType type,
+    int fs,
+    double fc,
+    double Q,
+    double peakGain
+) : b(biquad_create(type, fs, fc, Q, peakGain), biquad_destroy) {
+    if (!b) throw std::bad_alloc();
+}
+
+// copy constructor
+biquadFilter::biquadFilter(const biquadFilter& other) :
+b(biquad_clone(other.b.get()), biquad_destroy) {
+    if (!b) throw std::bad_alloc();
+}
+
+// assignment overload
+biquadFilter& biquadFilter::operator=(const biquadFilter& other) {
+    if (this != &other) {
+        b.reset(biquad_clone(other.b.get()));
+        if (!b) throw std::bad_alloc();
+    }
+    return *this;
+}
+
+// calculate magnitude
+double biquadFilter::magnitude(double f) const {
+    return biquad_magnitude(b.get(), f);
+}
+
+// process sample
+bqFloat biquadFilter::process(bqFloat x) {
+    return biquad_process(b.get(), x);
+}
+
+// set/get filter parameter - fs
+void biquadFilter::setFs(double fs) {
+    return biquad_setFs(b.get(), fs);
+}
+double biquadFilter::getFs(void) const {
+    return biquad_getFs(b.get());
+}
+
+// set/get filter parameter - type
+void biquadFilter::setType(bqFilterType type) {
+    biquad_setType(b.get(), type);
+}
+bqFilterType biquadFilter::getType(void) const {
+return biquad_getType(b.get());
+}
+
+// set/get filter parameter - fc
+void biquadFilter::setFc(double fc) {
+    biquad_setFc(b.get(), fc);
+}
+double biquadFilter::getFc(void) const {
+    return biquad_getFc(b.get());
+}
+
+// set/get filter parameter - Q
+void biquadFilter::setQ(double Q) {
+    biquad_setQ(b.get(), Q);
+}
+double biquadFilter::getQ(void) const {
+    return biquad_getQ(b.get());
+}
+
+// set/get filter parameter - peakGain
+void biquadFilter::setPeakGain(double peakGain) {
+    biquad_setPeakGain(b.get(), peakGain);
+}
+double biquadFilter::getPeakGain(void) const {
+    return biquad_getPeakGain(b.get());
+}
+
+// clear buffers
+void biquadFilter::clear(void) {
+    biquad_clear(b.get());
+}
 
 #endif // BIQUADS_HPP_
